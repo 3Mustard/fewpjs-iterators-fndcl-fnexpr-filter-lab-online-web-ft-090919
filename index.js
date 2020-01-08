161581ff8d19afbf0a21294531bdbaafd9e1653b
Matthew C <m23.cullen@gmail.com>
2020-01-08 15:40:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
  function findMatching(drivers,stringArg){
-   drivers.forEach(driver => driver.toLowercase)
+   drivers.forEach(driver => driver.toLowerCase());
+   stringArg.toLowerCase();
+   drivers.filter(driver => {return driver === stringArg});
  }
