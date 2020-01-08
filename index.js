f7bee4f142d3ab4da5e3f6a7ff1c59e47325f75c
Matthew C <m23.cullen@gmail.com>
2020-01-08 15:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
  function findMatching(drivers,stringArg){
    return drivers.filter(driver => driver.toLowerCase() === stringArg.toLowerCase());
  }
+ 
+ function matchName(drivers,stringArg){
+   //drivers are objects with name: key
+   //return drivers names who match the argument
+   return drivers.filter(driver => driver.name === stringArg)
+ }
